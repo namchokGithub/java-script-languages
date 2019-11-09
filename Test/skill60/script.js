374512455d10db5da0f484129d6789264acf99d4
namchokGithub <pech4751@gmail.com>
2019-11-09 11:15:52 +0700

    add file script

---
+++ Test/skill60/script.js
@@ -0,0 +1,17 @@
+console.log("Hello team 2");
+// Nav bar -------------------------------------------------------------------------//
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+//------------------------------------------------------------------------------------//
